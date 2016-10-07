9ca31e63201f946cfef53383439e3acb06d4f73a
heinrich_hoza <poseidon.guy@gmail.com>
2016-10-06 21:58:49 -0700

    pw update

--- download.php
+++ download.php
@@ -10,20 +10,20 @@ if (!isset($id) || !is_numeric($id))
 $id = intval($id);
 
 $db_username = 'audioid';
-$db_password = '_music_now_';
+$db_password = 'useless_password';
 include_once('php/utils/startup_connections.php');
 
-$query  = 'SELECT title, filename FROM songs WHERE id=' . $id . ';';
+$query  = 'SELECT name, filename FROM songs WHERE id=' . $id . ';';
 $result = $sql_link->query($query);
 
 if ($result === false)
-  die('query "' . $query . '" died: <br />' . $sql_link->error);
+  die('query "' . $query . '" died: <br />' . $sql_link->errorInfo()[2]);
 
 if ($result->num_rows != 1)
   die('invalid id');
 
-$tmp      = $result->fetch_array(MYSQL_ASSOC);
-$title    = $tmp['title'];
+$tmp      = $result->fetch(PDO:FETCH_ASSOC);
+$title    = $tmp['name'];
 $filename = $tmp['filename'];
 
 header('Content-Disposition: attachment; filename="' . strtolower($title) . '.mp3"');
