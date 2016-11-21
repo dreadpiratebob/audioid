5f7743e2c5613fc5a565027685d9cfb743aab214
Heinrich Hoza <Heinrich Hoza>
2016-11-21 00:16:01 -0800

    changing the song's 'title' column to 'name' in the 'queue album' command to match what's now in the db.

--- php/commands/now_playing/queue_album.php
+++ php/commands/now_playing/queue_album.php
@@ -8,19 +8,19 @@ function queue_album_up($data, $sql_link)
     return "data error (error code 343)";
   
   $id        = intval($id);
-  $query     = "SELECT s.id AS id, s.title AS title, s.filename AS filename, GROUP_CONCAT(ar.name, s_ar.conjunction ORDER BY s_ar.list_order SEPARATOR '') AS artist\n" .
+  $query     = "SELECT s.id AS id, s.name AS title, s.filename AS filename, GROUP_CONCAT(ar.name, s_ar.conjunction ORDER BY s_ar.list_order SEPARATOR '') AS artist\n" .
                "FROM songs AS s\n" .
                "  INNER JOIN songs_albums AS s_al ON s_al.song_id = s.id\n" .
                "  INNER JOIN albums       AS al   ON al.id = s_al.album_id\n" .
                "    AND al.id = $id\n" .
                "  LEFT JOIN songs_artists AS s_ar ON s_ar.song_id = s.id\n" .
                "  LEFT JOIN artists AS ar ON ar.id = s_ar.artist_id\n" .
-               "GROUP BY s.id, s.title, s.filename\n" . // gotta have 3 columns here 'cause i can't select columns that i'm not also grouping by.  also, i have to have the 'group by' clause after the 'where' clause.
+               "GROUP BY s.id, s.name, s.filename\n" . // gotta have 3 columns here 'cause i can't select columns that i'm not also grouping by.  also, i have to have the 'group by' clause after the 'where' clause.
                "ORDER BY s_al.track_number;";
   $sql_album = $sql_link->query($query);
   
   if ($sql_album === false)
-    return 'data error (error code 344)';
+    return 'data error (error code 344: ' . $sql_link->errorInfo()[2] . ')';
   
   for ($i = 0; $i < $sql_album->rowCount(); ++$i)
   {
