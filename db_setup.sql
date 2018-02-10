27470f8a79e548c3ab9a5a07cf168f47cc2428e6
Unknown <heinrichhoza1.02@gmail.com>
2018-02-10 01:42:07 -0800

    adding SQL
    adding a user and two stored procedures, and granting execute permissions to that user on the stored procedures.

--- db_setup.sql
+++ db_setup.sql
@@ -92,3 +92,67 @@ CREATE TABLE songs_genres
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
+
+# change the password here.
+CREATE USER 'audioid_user'@'localhost' IDENTIFIED BY 'some password';
+
+DELIMITER //
+
+CREATE PROCEDURE get_catalog_properties(IN in_catalog_id INT(64) UNSIGNED, OUT out_catalog_name varchar(64), OUT out_catalog_base_path varchar(1024), OUT dbg INT)
+BEGIN
+  SELECT name, base_path INTO out_catalog_name, out_catalog_base_path FROM catalogs WHERE id = in_catalog_id;
+  SELECT COUNT(id) INTO dbg FROM catalogs;
+END//
+
+DELIMITER ;
+
+GRANT EXECUTE ON PROCEDURE audioid.get_catalog_properties TO 'audioid_user'@'localhost';
+
+DELIMITER //
+
+CREATE PROCEDURE delete_catalog(IN in_catalog_id INT(64) UNSIGNED)
+BEGIN
+  START TRANSACTION;
+  
+  DELETE FROM songs_artists
+  WHERE song_id IN
+  (
+    SELECT id
+    FROM songs
+    WHERE songs.catalog_id = in_catalog_id
+  );
+  
+  DELETE FROM artists WHERE id NOT IN
+  (
+    SELECT DISTINCT s_a.artist_id
+    FROM songs_artists AS s_a
+    INNER JOIN songs AS s ON s.id = s_a.song_id
+      AND s.catalog_id != in_catalog_id
+  );
+  
+  DELETE FROM songs_albums
+  WHERE song_id IN
+  (
+    SELECT id
+    FROM songs
+    WHERE songs.catalog_id = in_catalog_id
+  );
+  
+  DELETE FROM albums WHERE id NOT IN
+  (
+    SELECT s_a.album_id
+    FROM songs_albums AS s_a
+    INNER JOIN songs AS s ON s.id = s_a.song_id
+      AND s.catalog_id != in_catalog_id
+  );
+  
+  DELETE FROM songs WHERE catalog_id = in_catalog_id;
+  
+  DELETE FROM catalogs WHERE id = in_catalog_id;
+  
+  COMMIT;
+END//
+
+DELIMITER ;
+
+GRANT EXECUTE ON PROCEDURE audioid.delete_catalog TO 'audioid_user'@'localhost';
