79ded61d91d88d8b4dfb00d41d41623d966654f5
Unknown <heinrichhoza1.02@gmail.com>
2018-02-12 22:10:24 -0800

    PHP/SQL updates
    updating the 'delete catalog' command to use a stored procedure.

--- php/commands/catalog_editor/delete.php
+++ php/commands/catalog_editor/delete.php
@@ -2,7 +2,6 @@
 
 function delete_catalog($data, $sql_link)
 {
-  //// data validation & cleanup ////
   $cat_id = $data['cat_id'];
   $fb = '';
   
@@ -10,76 +9,7 @@ function delete_catalog($data, $sql_link)
     return 'data error (error code 36)<br />';
   
   $cat_id = intval($cat_id);
-  
-  //// end data validation & cleanup ////
-  
-  //// deal w/ artists ////
-  $query  = "DELETE FROM songs_artists\n" .
-            "WHERE song_id IN\n" .
-            "(\n" .
-            "  SELECT id\n" .
-            "  FROM songs\n" .
-            '  WHERE songs.catalog_id = ' . $cat_id . "\n" .
-            ');';
-  $result = $sql_link->query($query);
-  if ($result === false)
-    die('this query query died:<br />' . str_replace("\n", "<br />\n", str_replace(' ', '&nbsp;', $query)) . "<br />\nwith this error: " . $sql_link->errorInfo()[2]);
-  
-  $query  = "DELETE FROM artists WHERE id NOT IN\n" .
-            "(\n" .
-            "  SELECT DISTINCT s_a.artist_id\n" .
-            "  FROM songs_artists AS s_a\n" .
-            "  INNER JOIN songs AS s ON s.id = s_a.song_id\n" .
-            '    AND s.catalog_id != ' . $cat_id . "\n" .
-            ');';
-  $result = $sql_link->query($query);
-  if ($result === false)
-    die('this query query died:<br />' . str_replace("\n", "<br />\n", str_replace(' ', '&nbsp;', $query)) . "<br />\nwith this error: " . $sql_link->errorInfo()[2]);
-  
-  //// deal w/ albums ////
-  $query  = "DELETE FROM songs_albums\n" .
-            "WHERE song_id IN\n" .
-            "(\n" .
-            "  SELECT id\n" .
-            "  FROM songs\n" .
-            '  WHERE songs.catalog_id = ' . $cat_id . "\n" .
-            ');';
-  $result = $sql_link->query($query);
-  if ($result === false)
-    die('this query query died:<br />' . str_replace("\n", "<br />\n", str_replace(' ', '&nbsp;', $query)) . "<br />\nwith this error: " . $sql_link->errorInfo()[2]);
-  
-  $query  = "DELETE FROM albums WHERE id NOT IN\n" .
-            "(\n" .
-            "  SELECT s_a.album_id\n" .
-            "  FROM songs_albums AS s_a\n" .
-            "  INNER JOIN songs AS s ON s.id = s_a.song_id\n" .
-            '    AND s.catalog_id!= ' . $cat_id . "\n" .
-            ');';
-  $result = $sql_link->query($query);
-  if ($result === false)
-    die('this query query died:<br />' . str_replace("\n", "<br />\n", str_replace(' ', '&nbsp;', $query)) . "<br />\nwith this error: " . $sql_link->errorInfo()[2]);
-  
-  
-  //// deal w/ songs ////
-  $query  = 'DELETE FROM songs WHERE catalog_id=' . $cat_id . ';';
-  $result = $sql_link->query($query);
-  if ($result === false)
-    die('query "' . $query . '" died: ' . $sql_link->errorInfo()[2]);
-  
-  //// deal w/ genres ////
-  $query  = "DELETE FROM genres WHERE id IN\n" .
-            "(\n" .
-            "  SELECT s_g.genre_id\n" .
-            "  FROM songs_genres AS s_g\n" .
-            "    INNER JOIN songs AS s ON s.id = s_g.song_id\n" .
-            "  WHERE s.catalog_id = $cat_id\n" .
-            ');';
-  $result = $sql_link->query($query);
-  if ($result === false)
-    die('this query query died:<br />' . str_replace("\n", "<br />\n", str_replace(' ', '&nbsp;', $query)) . "<br />\nwith this error: " . $sql_link->errorInfo()[2]);
-  
-  //// delete the catalog ////
-  $query  = 'DELETE FROM catalogs WHERE id=' . $cat_id . ';';
+  $query  = "call delete_catalog({$cat_id});"; // i'm trusting PHP's 'intval' function here to clean out any bad data and to make MySQL's cleaning unnecessary.
   $result = $sql_link->query($query);
   if ($result === false)
     die('query "' . $query . '" died: ' . $sql_link->errorInfo()[2]);
