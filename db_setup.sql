42dc8234cd4d237026360229a5afdd235e477ee7
Unknown <heinrichhoza1.02@gmail.com>
2018-02-14 02:07:20 -0800

    SQL update
    bug fix

--- db_setup.sql
+++ db_setup.sql
@@ -98,7 +98,12 @@ CREATE USER 'audioid_user'@'localhost' IDENTIFIED BY 'some password';
 CREATE USER 'audioid_admin'@'localhost' IDENTIFIED BY 'other password';
 
 GRANT SELECT ON audioid.catalogs TO 'audioid_admin'@'localhost';
-GRANT SELECT ON audioid.songs, audioid.artists, audioid.albums, audioid.genres, audioid.catalogs TO 'audioid_user'@'localhost';
+
+GRANT SELECT ON audioid.artists  TO 'audioid_user'@'localhost';
+GRANT SELECT ON audioid.albums   TO 'audioid_user'@'localhost';
+GRANT SELECT ON audioid.catalogs TO 'audioid_user'@'localhost';
+GRANT SELECT ON audioid.genres   TO 'audioid_user'@'localhost';
+GRANT SELECT ON audioid.songs    TO 'audioid_user'@'localhost';
 
 DELIMITER //
 
