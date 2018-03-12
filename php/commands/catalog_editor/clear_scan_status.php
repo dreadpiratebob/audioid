6335c4249bf0ea83315db0877d6fb2e9e38bca06
Unknown <heinrichhoza1.02@gmail.com>
2018-03-12 01:40:42 -0700

    PHP update
    pulling a common function out of two files.

--- php/commands/catalog_editor/clear_scan_status.php
+++ php/commands/catalog_editor/clear_scan_status.php
@@ -1,28 +1,9 @@
 <?php
 
-function write_log()
-{
-  if (!isset($_SESSION['scan_status']) || !is_array($_SESSION['scan_status']) || count($_SESSION['scan_status']) == 0)
-    return;
-  
-  $name       = $_SESSION['scan_status']['name'];
-  $start_time = $_SESSION['scan_status']['start_time'];
-  $log        = 'catalog "' . $name . '" scanned from ' . $_SERVER['REMOTE_ADDR'] . '; started at ' . $start_time . ".\n";
-  for ($i = 0; $i < count($_SESSION['scan_status']); $i++)
-  {
-    $line = $_SESSION['scan_status'][$i] . "\n";
-    $line = preg_replace("/[^\n-þ]/", '?', $line);
-    $log .= $line;
-  }
-  
-  $result = file_put_contents('/var/log/my_music/scans/' . $name . ' ' . $start_time . '.log', $log);
-  if ($result === false)
-    $_SESSION['scan_status'][count($_SESSION['scan_status'])] = 'failed to write log.';
-}
+include_once('php/utils/catalog_scanner/status_functions.php');
 
-write_log();
+$feedback .= write_log();
 
 unset($_SESSION['scan_status']);
-echo $_SESSION['scan_status'];
 
 ?>
--- php/utils/catalog_scanner/status_functions.php
+++ php/utils/catalog_scanner/status_functions.php
@@ -4,6 +4,16 @@ function add_status($new_status, $session_id, $dump_to_screen = true)
 {
   session_start($session_id);
   
+  if (!array_key_exists('scan_log', $_SESSION))
+  {
+    $_SESSION['scan_log'] = array();
+  }
+  
+  if (!array_key_exists('scan_status', $_SESSION))
+  {
+    $_SESSION['scan_status'] = array();
+  }
+  
   $lines = explode('\n', $new_status);
   
   for ($i = 0; $i < count($lines); ++$i)
@@ -24,6 +34,31 @@ function dump_query($title, $query, $params, $session_id, $dump_to_screen = fals
   
   $log_message = "\n\n----query dump: {$title} ----\nquery w/ params:\n{$query}\n\nquery w/o params:\n{$deparamed_query}\n\nparams:\n" . print_r($params, true) . "----end query dump: {$title} ----\n\n";
   add_status($log_message, $session_id, $dump_to_screen);
+}
+
+function write_log()
+{
+  if (!isset($_SESSION['scan_status']) || !is_array($_SESSION['scan_status']) || count($_SESSION['scan_status']) == 0)
+    return 'got no log to write.';
+  
+  $name       = $_SESSION['scan_status']['name'];
+  $start_time = $_SESSION['scan_status']['start_time'];
+  $log        = 'catalog "' . $name . '" scanned from ' . $_SERVER['REMOTE_ADDR'] . '; started at ' . $start_time . ".\n";
+  for ($i = 0; $i < count($_SESSION['scan_log']); $i++)
+  {
+    $line = $_SESSION['scan_log'][$i] . "\n";
+    $line = preg_replace("/[^\n-þ]/", '?', $line);
+    $log .= $line;
+  }
+  
+  $result = file_put_contents("/web/log/scans/{$name} {$start_time}.log", $log);
+  if ($result === false)
+  {
+    $_SESSION['scan_status'][count($_SESSION['scan_status'])] = 'failed to write log.';
+    return 'failed to write log.';
+  }
+  
+  return 'wrote the log.';
 }
 
 ?>
