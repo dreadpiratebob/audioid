b08a91723c608796d630c80914639e1d49f6342a
dreadpiratebob <poseidon.guy@gmail.com>
2024-02-14 19:52:44 -0800

    moving the api to its own repo.

--- api/logic/artists.py
+++
@@ -1,7 +0,0 @@
-from api.dao.load_db_models import get_artists as get_db_artists
-from api.util.audioid.artists import IncludeAlbumModes
-from api.util.response_list_modifiers import FilterInfo, OrderByCol, PageInfo
-
-def get_artists(catalog_id:int, artist_filter:FilterInfo, genre_filter:FilterInfo, order_by:list[OrderByCol], page_info:PageInfo,
-                include_songs:bool = False, include_albums:IncludeAlbumModes = IncludeAlbumModes.ALBUM_ARTIST_ONLY, include_album_tracks:bool = False, include_genres:bool = False):
-  return get_db_artists(catalog_id, artist_filter, genre_filter, order_by, page_info, include_songs, include_albums, include_album_tracks, include_genres)
--- api/logic/genres.py
+++
@@ -1,5 +0,0 @@
-from api.dao.load_db_models import get_genres as get_db_genres
-from api.util.response_list_modifiers import FilterInfo, OrderByCol, PageInfo
-
-def get_genres(catalog_id:int, genre_filter:FilterInfo, order_by:list[OrderByCol], page_info:PageInfo):
-  return get_db_genres(catalog_id, genre_filter, order_by, page_info)
